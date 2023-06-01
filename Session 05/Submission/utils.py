f04374e77a2cd0093a0f692c7852feda5985ace9
Viraj Noorithaya <8409854+nviraj@users.noreply.github.com>
2023-06-02 00:55:24 +0530

    Added comments to train and test

--- Session 05/Submission/utils.py
+++ Session 05/Submission/utils.py
@@ -69,35 +69,46 @@ def train(model, device, train_loader, optimizer, criterion):
     Function to train the model on the train dataset.
     """
 
+    # Train the model
     model.train()
+    # Initialize progress bar
     pbar = tqdm(train_loader)
 
+    # Reset the loss and correct predictions for the epoch
     train_loss = 0
     correct = 0
     processed = 0
 
+    # Iterate over the train loader
     for batch_idx, (data, target) in enumerate(pbar):
+        # Move data and labels to device
         data, target = data.to(device), target.to(device)
+        # Clear the gradients for the optimizer to avoid accumulation
         optimizer.zero_grad()
 
         # Predict
         pred = model(data)
 
-        # Calculate loss
+        # Calculate loss for the batch
         loss = criterion(pred, target)
+        # Update the loss
         train_loss += loss.item()
 
-        # Backpropagation
+        # Backpropagation to calculate the gradients
         loss.backward()
+        # Update the weights
         optimizer.step()
 
+        # Get the count of correct predictions
         correct += GetCorrectPredCount(pred, target)
         processed += len(data)
 
+        # Update the progress bar
         pbar.set_description(
             desc=f"Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}"
         )
 
+    # Append the final loss and accuracy for the epoch
     train_acc.append(100 * correct / processed)
     train_losses.append(train_loss / len(train_loader))
 
@@ -107,23 +118,32 @@ def test(model, device, test_loader, criterion):
     Function to test the model on the test dataset.
     """
 
+    # Test the model
     model.eval()
 
+    # Reset the loss and correct predictions for the epoch
     test_loss = 0
     correct = 0
 
+    # Disable gradient calculation while testing
     with torch.no_grad():
         for batch_idx, (data, target) in enumerate(test_loader):
+            # Move data and labels to device
             data, target = data.to(device), target.to(device)
 
+            # Predict using model
             output = model(data)
+            # Calculate loss for the batch
             test_loss += criterion(
                 output, target
             ).item()  # Remove reduction and fix batch loss
 
+            # Get the count of correct predictions
             correct += GetCorrectPredCount(output, target)
 
+    # Calculate the final loss
     test_loss /= len(test_loader.dataset)
+    # Append the final loss and accuracy for the epoch
     test_acc.append(100.0 * correct / len(test_loader.dataset))
     test_losses.append(test_loss)
 
@@ -144,6 +164,7 @@ def plot_train_test_metrics():
       A matplotlib figure.
     """
 
+    # Plot the graphs in a 2x2 grid showing the training and test metrics
     fig, axs = plt.subplots(2, 2, figsize=(15, 10))
     axs[0, 0].plot(train_losses)
     axs[0, 0].set_title("Training Loss")
