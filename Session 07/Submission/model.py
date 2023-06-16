42f9cd61d72fbb32d778ab857f98b51d0121b623
Viraj Noorithaya <8409854+nviraj@users.noreply.github.com>
2023-06-16 15:01:51 +0530

    Changed class names to pascal style, basic skeleton added

--- Session 07/Submission/model.py
+++ Session 07/Submission/model.py
@@ -116,10 +116,10 @@ def plot_train_test_metrics(train_losses, train_acc, test_losses, test_acc):
 
 # Define the neural network architecture
 # This is for Model 00
-class Model_00(nn.Module):
+class Model00(nn.Module):
     # This defines the structure of the NN.
     def __init__(self):
-        super(Model_00, self).__init__()
+        super(Model00, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
@@ -147,3 +147,114 @@ class Model_00(nn.Module):
         # print(x.shape)
 
         return F.log_softmax(x, dim=1)
+
+
+class Model01(nn.Module):
+    # This defines the structure of the NN.
+    def __init__(self):
+        super(Model01, self).__init__()
+
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 24
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 22
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2)  # output_size = 11
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=32,
+                kernel_size=(1, 1),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 11
+
+        # CONVOLUTION BLOCK 2
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 9
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=(3, 3),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 7
+
+        # OUTPUT BLOCK
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=10,
+                kernel_size=(1, 1),
+                padding=0,
+                bias=False,
+            ),
+            nn.ReLU(),
+        )  # output_size = 7
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=10,
+                out_channels=10,
+                kernel_size=(7, 7),
+                padding=0,
+                bias=False,
+            ),
+            # nn.ReLU() NEVER!
+        )  # output_size = 1 7x7x10 | 7x7x10x10 | 1x1x10
+
+    def forward(self, x):
+        """Forward pass"""
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x = self.convblock3(x)
+        x = self.pool1(x)
+        x = self.convblock4(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.convblock8(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
