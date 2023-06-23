1728ed51b2fc3819cb9b581a127e19399666164b
Viraj Noorithaya <8409854+nviraj@users.noreply.github.com>
2023-06-23 21:03:47 +0530

    Fix code visulaising sample images

--- Session 08/Submission/visualize.py
+++ Session 08/Submission/visualize.py
@@ -26,6 +26,47 @@ from PIL import Image
 #     return fig
 
 
+# def plot_sample_training_images(batch_data, batch_label, class_label, num_images=30):
+#     """Function to plot sample images from the training data."""
+#     images, labels = batch_data, batch_label
+
+#     # Calculate the number of images to plot
+#     num_images = min(num_images, len(images))
+#     # calculate the number of rows and columns to plot
+#     num_cols = 5
+#     num_rows = int(np.ceil(num_images / num_cols))
+
+#     # Initialize a subplot with the required number of rows and columns
+#     fig, axs = plt.subplots(num_rows, num_cols, figsize=(10, 10))
+
+#     # Iterate through the images and plot them in the grid along with class labels
+
+#     for i in range(1, num_images + 1):
+#         plt.subplot(num_rows, num_cols, i)
+#         plt.tight_layout()
+#         plt.axis("off")
+#         plt.imshow(images[i - 1])
+#         plt.title(class_label[labels[i - 1].item()])
+#         plt.xticks([])
+#         plt.yticks([])
+
+#     return fig, axs
+
+
+def convert_back_image(image):
+    """Using mean and std deviation convert image back to normal"""
+    cifar10_mean = (0.4914, 0.4822, 0.4471)
+    cifar10_std = (0.2469, 0.2433, 0.2615)
+    image = image.numpy().astype(dtype=np.float32)
+
+    for i in range(image.shape[0]):
+        image[i] = (image[i] * cifar10_std[i]) + cifar10_mean[i]
+
+    image = image.clip(0, 1)
+
+    return np.transpose(image, (1, 2, 0))
+
+
 def plot_sample_training_images(batch_data, batch_label, class_label, num_images=30):
     """Function to plot sample images from the training data."""
     images, labels = batch_data, batch_label
@@ -39,20 +80,16 @@ def plot_sample_training_images(batch_data, batch_label, class_label, num_images
     # Initialize a subplot with the required number of rows and columns
     fig, axs = plt.subplots(num_rows, num_cols, figsize=(10, 10))
 
-    for i in range(num_rows):
-        for j in range(num_cols):
-            index = i * num_cols + j
-            if index < num_images:
-                axs[i, j].axis("off")
-                tmp_img = images[index]
+    # Iterate through the images and plot them in the grid along with class labels
 
-                image = np.transpose(
-                    images[index], (1, 2, 0)
-                )  # Transpose image dimensions if needed
-                axs[i, j].imshow(image)
-                axs[i, j].set_title(class_label[labels[index].item()])
-
-    plt.tight_layout()
+    for img_index in range(1, num_images + 1):
+        plt.subplot(num_rows, num_cols, img_index)
+        plt.tight_layout()
+        plt.axis("off")
+        plt.imshow(convert_back_image(images[img_index - 1]))
+        plt.title(class_label[labels[img_index - 1].item()])
+        plt.xticks([])
+        plt.yticks([])
 
     return fig, axs
 
