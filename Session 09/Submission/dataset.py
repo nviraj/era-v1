92f421e27e00ef91401818e0468a22efcc9d7314
Viraj Noorithaya <8409854+nviraj@users.noreply.github.com>
2023-06-30 17:12:03 +0530

    Class to apply transformation from albumentations

--- Session 09/Submission/dataset.py
+++ Session 09/Submission/dataset.py
@@ -1,5 +1,39 @@
+"""This file contains functions to download and transform the CIFAR10 dataset"""
 import numpy as np
+from torch.utils.data import Dataset
 from torchvision import datasets
+
+
+class CIFAR10Transforms(Dataset):
+    """Apply albumentations augmentations to CIFAR10 dataset"""
+
+    # Given a dataset and transformations,
+    # apply the transformations and return the dataset
+    def __init__(self, dataset, transforms):
+        self.transforms = transforms
+        self.dataset = dataset
+
+    def __getitem__(self, idx):
+        # Get the image and label from the dataset
+        image, label = self.dataset[idx]
+
+        # Apply transformations on the image
+        image = self.transforms(image=np.array(image))["image"]
+
+        return image, label
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __repr__(self):
+        return (
+            f"CIFAR10Transforms(dataset={self.dataset}, transforms={self.transforms})"
+        )
+
+    def __str__(self):
+        return (
+            f"CIFAR10Transforms(dataset={self.dataset}, transforms={self.transforms})"
+        )
 
 
 def split_cifar_data(data_path, train_transforms, test_transforms):
@@ -7,13 +41,21 @@ def split_cifar_data(data_path, train_transforms, test_transforms):
     Function to download the MNIST data
     """
 
-    # Download MNIST dataset and apply transformations
-    train_data = datasets.CIFAR10(
-        data_path, train=True, download=True, transform=train_transforms
-    )
-    test_data = datasets.CIFAR10(
-        data_path, train=False, download=True, transform=test_transforms
-    )
+    # Download MNIST dataset
+    train_data = datasets.CIFAR10(data_path, train=True, download=True)
+    test_data = datasets.CIFAR10(data_path, train=False, download=True)
+
+    # Calculate and print the mean and standard deviation of the dataset
+    mean, std = calculate_mean_std(train_data)
+    print(f"Mean: {mean}")
+    print(f"Std: {std}\n")
+
+    # Apply transforms on the dataset
+    # Use the above class to apply transforms on the dataset using albumentations
+    train_data = CIFAR10Transforms(train_data, train_transforms)
+    test_data = CIFAR10Transforms(test_data, test_transforms)
+
+    print("Transforms applied on the dataset\n")
 
     return train_data, test_data
 
