100d78bd8e26c5570d6a47644cdb9c59f1b46ff4
Viraj Noorithaya <8409854+nviraj@users.noreply.github.com>
2023-08-04 17:46:17 +0530

    Validation split is created only if required where val_split is > 0

--- Session 12/Submission/modules/lightning_dataset.py
+++ Session 12/Submission/modules/lightning_dataset.py
@@ -1,24 +1,22 @@
 """This file contains functions to prepare dataloader in the way lightning expects"""
 import pytorch_lightning as pl
 import torchvision.datasets as datasets
+from lightning_fabric.utilities.seed import seed_everything
 from modules.dataset import CIFAR10Transforms, apply_cifar_image_transformations
-from pytorch_lightning.utilities.seed import seed_everything
 from torch.utils.data import DataLoader, random_split
 
 
 class CIFARDataModule(pl.LightningDataModule):
     """Lightning DataModule for CIFAR10 dataset"""
 
-    def __init__(self, data_path, batch_size, num_workers, seed):
+    def __init__(self, data_path, batch_size, num_workers, seed, val_split=0):
         super().__init__()
 
         self.data_path = data_path
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.seed = seed
-        self.training_dataset = None
-        self.validation_dataset = None
-        self.testing_dataset = None
+        self.val_split = val_split
         self.dataloader_dict = {
             "shuffle": True,
             "batch_size": self.batch_size,
@@ -27,18 +25,18 @@ class CIFARDataModule(pl.LightningDataModule):
             "worker_init_fn": self._init_fn,
         }
 
-    def _split_train_val(self, dataset, val_split=0.2):
+    def _split_train_val(self, dataset):
         """Split the dataset into train and validation sets"""
 
         # Throw an error if the validation split is not between 0 and 1
-        if not 0 < val_split < 1:
+        if not 0 < self.val_split < 1:
             raise ValueError("Validation split must be between 0 and 1")
 
-        # Set seed again, might not be necessary
-        seed_everything(int(self.seed))
+        # # Set seed again, might not be necessary
+        # seed_everything(int(self.seed))
 
         # Calculate lengths of each dataset
-        train_length = int((1 - val_split) * len(dataset))
+        train_length = int((1 - self.val_split) * len(dataset))
         val_length = len(dataset) - train_length
 
         # Split the dataset
@@ -55,22 +53,29 @@ class CIFARDataModule(pl.LightningDataModule):
     # https://lightning.ai/docs/pytorch/stable/data/datamodule.html#setup
     # https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.core.hooks.DataHooks.html#lightning.pytorch.core.hooks.DataHooks.setup
     def setup(self, stage=None):
-        seed_everything(int(self.seed))
+        # seed_everything(int(self.seed))
+        # self.prepare_data()
         # Define the data transformations
         train_transforms, test_transforms = apply_cifar_image_transformations()
         val_transforms = test_transforms
 
         # Load the appropriate CIFAR10 dataset
-        if stage in ["fit", None, "validate"]:
-            full_data = datasets.CIFAR10(self.data_path, train=True)
-            data_train, data_val = self._split_train_val(full_data, val_split=0.2)
+        full_data = datasets.CIFAR10(self.data_path, train=True)
+
+        # Assign Test split(s) for use in Dataloaders
+        data_test = datasets.CIFAR10(self.data_path, train=False)
+        self.testing_dataset = CIFAR10Transforms(data_test, test_transforms)
+
+        # Create train and validation datasets
+        if self.val_split != 0:
+            data_train, data_val = self._split_train_val(full_data)
 
             self.training_dataset = CIFAR10Transforms(data_train, train_transforms)
             self.validation_dataset = CIFAR10Transforms(data_val, val_transforms)
-
-        # Assign Test split(s) for use in Dataloaders
-        if stage == "test":
-            self.testing_dataset = CIFAR10Transforms(datasets.CIFAR10(self.data_path, train=False), test_transforms)
+        else:
+            # Only training data here
+            self.training_dataset = full_data
+            self.validation_dataset = self.testing_dataset
 
     # https://lightning.ai/docs/pytorch/stable/data/datamodule.html#train-dataloader
     def train_dataloader(self):
